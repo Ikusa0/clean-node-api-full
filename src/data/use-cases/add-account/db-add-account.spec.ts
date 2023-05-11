bd205a4032cdd91187c7204f8e9c72b57fa7e989
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-05-11 18:49:28 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -13,10 +13,10 @@ const makeEncrypter = (): Encrypter => {
 
 const makeAddAccountRepository = (): AddAccountRepository => {
   class AddAccountRepositoryStub implements AddAccountRepository {
-    async add (account: AddAccountModel): Promise<AccountModel> {
+    async add (accountData: AddAccountModel): Promise<AccountModel> {
       const fakeAccount = {
         id: 'valid_id',
-        ...account
+        ...accountData
       }
       return new Promise(resolve => { resolve(fakeAccount) })
     }
@@ -97,5 +97,18 @@ describe('DbAddAccount Usecase', () => {
 
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const account = await sut.add(accountData)
+    expect(account).toEqual({ id: 'valid_id', ...accountData, password: 'hashed_password' })
   })
 })
